16a1eade36b32db9b2be48c7d08be27046c405b5
unknown <you@example.com>
2023-12-30 19:19:49 +0600

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+# from django import forms
+# from .models import Comments
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comments
+#         fields = ['name', 'email', 'body']
+
+from django import forms
+
+from .models import Comments
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ['name', 'email', 'body']
